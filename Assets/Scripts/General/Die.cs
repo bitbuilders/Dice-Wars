14b0853c64c180c8e7c7f39c78c50551da1048dc
Colin Misbach <colin@misbach.com>
2018-04-11 20:35:53 -0600

    :+1: Initial Bulk Commit

---
+++ Assets/Scripts/General/Die.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Die : MonoBehaviour
+{
+    [SerializeField] [Range(1.0f, 10.0f)] float m_jumpResistance = 3.0f;
+    [SerializeField] [Range(1.0f, 10.0f)] float m_fallMultiplier = 3.0f;
+
+    Rigidbody m_rigidbody;
+
+    private void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (m_rigidbody.velocity.y > 0.0f)
+        {
+            m_rigidbody.velocity += (Vector3.up * Physics.gravity.y) * (m_jumpResistance - 1.0f) * Time.deltaTime;
+        }
+        else if (m_rigidbody.velocity.y < 0.0f)
+        {
+            m_rigidbody.velocity += (Vector3.up * Physics.gravity.y) * (m_fallMultiplier - 1.0f) * Time.deltaTime;
+        }
+    }
+}
